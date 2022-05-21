be28806fc772412aede383a53be6ad623a181ac2
erikmcquarrie <McQuarrieErik@gmail.com>
2022-05-21 02:35:53 -0400

    test

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 app.use(express.static('public'));
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.urlencoded({ extended: true }));// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 
 //use method override
